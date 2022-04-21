8a1500dc159636ee29248134c5bf9552c583a8de
JoshBenPhilip <joshben4c@gmail.com>
2022-04-21 15:43:36 -0400

    commit 5 added npm and fetch

--- fetch.js
+++ fetch.js
@@ -1,25 +1,30 @@
 
 import fetch from 'node-fetch'
 
-async function getDataAsync() { 
-try{
-const response = await fetch('https://api.sampleapis.com/beers/ale')//get -> Promise
-const data = await response.json()
-console.log(data)
-} catch (err) {
-    console.error(err)
-}
-}
+// //let fname;
+// async function getDataAsync() { 
+// try{
+// const response = await fetch('https://api.sampleapis.com/beers/ale')//get -> Promise
+// const data = await response.json()
+// fname = data[ll].name
+// console.log("Hey what's up, " + fname +"?")
+// } catch (err) {
+//     console.error(err)
+// }
+// }
 
-getDataAsync()
+// getDataAsync()
 
-// // -----
+// // ----------toddversion .then .catch
 
 // fetch('https://api.sampleapis.com/beers/ale')
-// .then(
-//     (response => { 
-//         //do something
+// .then((response) => { 
+//     return response.json()    //do something
 //     })
+//     .then(data => {
+//         console.log(data [87])
+//    // do all the stuff we want to happen AFTER the fetch... always use dot. fetch
+// })
 // .catch((err) => {
 //     console.error(err)
 // })
@@ -29,7 +34,9 @@ getDataAsync()
 
 // // --------------
 
-// fetch('https://api.sampleapis.com/beers/ale')
-// .then((response) => {
+// tighter version of the above
+fetch('https://api.sampleapis.com/beers/ale')
+.then(response => response.json())
+.then(beers => console.log(beers [99]))
+.catch(err => console.error(err))
 
-// })
